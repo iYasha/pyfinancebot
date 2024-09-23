6708519cff4983d2bedbffbf0f1be56ea1edff8a
iYasha <33287747+iYasha@users.noreply.github.com>
2024-09-23 20:31:19 +0300

    fix(Monobank Integration): Wrapped integration webhook via try-except block

--- src/webserver/api/monobank/views.py
+++ src/webserver/api/monobank/views.py
@@ -1,6 +1,8 @@
+import sentry_sdk
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
+from config import bot
 from database import database
 from modules.integrations import MonobankIntegrationService, MonobankService
 from modules.integrations.monobank.schemas import WebhookRequest
@@ -21,19 +23,26 @@ async def create_operation_via_monobank(secret: str, webhook_data: WebhookReques
     integration = await MonobankIntegrationService.check_integration_by_secret(secret)
     if not integration:
         return JSONResponse(status_code=404, content={'detail': 'Not Found'})
-    account_id = webhook_data.data.account
-    account = await MonobankIntegrationService.get_account(integration, account_id)
-    if account is None:
-        async with database.transaction():
-            client_info = await MonobankService(integration.integration_key).get_client_info()
-            await MonobankIntegrationService.set_accounts(integration.chat_id, integration.company_id, client_info)
-            is_active_account = any([True for account in client_info.accounts if account.id == account_id])
-    else:
-        is_active_account = account.is_active
+    try:
+        account_id = webhook_data.data.account
+        account = await MonobankIntegrationService.get_account(integration, account_id)
+        if account is None:
+            async with database.transaction():
+                client_info = await MonobankService(integration.integration_key).get_client_info()
+                await MonobankIntegrationService.set_accounts(integration.chat_id, integration.company_id, client_info)
+                is_active_account = any([True for account in client_info.accounts if account.id == account_id])
+        else:
+            is_active_account = account.is_active
 
-    if is_active_account:
-        await MonobankIntegrationService.create_operation(
-            integration=integration,
-            webhook_data=webhook_data,
+        if is_active_account:
+            await MonobankIntegrationService.create_operation(
+                integration=integration,
+                webhook_data=webhook_data,
+            )
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        await bot.send_message(
+            integration.chat_id,
+            text='Не удалось создать операцию с помощью интеграции с Monobank.',
         )
     return JSONResponse(status_code=200, content={'detail': 'OK'})
